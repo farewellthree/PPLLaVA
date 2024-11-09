3cf8b8eca771896a798e6c3f0e41ed715eb927c1
farewellthree <1176569545@qq.com>
2024-11-09 22:55:54 +0800

    bug fix

--- script/inference/qabench/test_all.sh
+++ script/inference/qabench/test_all.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH="./:$PYTHONPATH"
-torchrun --nproc_per_node=8 --master_port=33009 stllm/test/qabench/qabench_infer_mp.py \
+torchrun --nproc_per_node=8 --master_port=33009 ppllava/test/qabench/qabench_infer_mp.py \
     --cfg-path config/vicuna/ppllava_vicuna7b_image_video.yaml \
     --ckpt-path /Path/to/ppllava_vicuna7b_image_video \
     --output_dir test_output/qabench/ppllava_vicuna7b_image_video \
